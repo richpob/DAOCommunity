ad701eea93a4958599bb052cf7b3a0783bd2a7d8
richpob <133718913+richpob@users.noreply.github.com>
2024-04-15 20:06:54 -0400

    cc

---
+++ migrations/2_deploy_community.js
@@ -0,0 +1,5 @@
+const Community = artifacts.require("Community");
+
+module.exports = function (deployer) {
+  deployer.deploy(Community, "Community Name", "Community Address", "President Name", "Admin Name", Date.now());
+};
